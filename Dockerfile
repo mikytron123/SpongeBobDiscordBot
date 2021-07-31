89ada0a0c16218607e59a502b701291b4078685b
Miky <vyasmiky@gmail.com>
2021-07-30 22:35:40 -0400

    added support for emojis

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . /usr/src/app
 RUN chmod +x bot.py
-RUN wget -O uni.ttf "https://img.download-free-fonts.com/dl.php?id=88978&hash=40d13c72f9bd682a8df865b946eb4e10" 
+RUN wget -O uni.ttf "https://img.download-free-fonts.com/dl.php?id=88978&hash=40d13c72f9bd682a8df865b946eb4e10"
+RUN wget -O color.ttf https://github.com/googlefonts/noto-emoji/blob/main/fonts/NotoEmoji-Regular.ttf?raw=true
 CMD ["python","-u", "bot.py"]
--- bot.py
+++ bot.py
@@ -6,18 +6,36 @@ from PIL import ImageDraw
 from discord.ext import commands
 from dotenv import load_dotenv
 from io import BytesIO
+from fontTools.ttLib import TTFont
 load_dotenv()
 import discord
 TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
+
+def has_glyph(font, glyph):
+    for table in font['cmap'].tables:
+        if ord(glyph) in table.cmap.keys():
+            return True
+    return False
 
 @bot.command(help="adds text to spongebob image")
 async def spongebob(ctx,arg):
     addstr = str(arg)
     img = Image.open("spongebob.jpg")
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("uni.ttf", 55)
-    draw.text((590, 20),addstr,(255,255,255),font=font)
+    startlen = 590
+    for char in addstr:
+        checkfont = TTFont('uni.ttf')
+        checkfont2 = TTFont('color.ttf')
+        if has_glyph(checkfont,char):
+            font = ImageFont.truetype("uni.ttf",55,encoding='unic')
+        elif has_glyph(checkfont2,char):
+            font = ImageFont.truetype("color.ttf",55)
+        else:
+            font = ImageFont.truetype("uni.ttf",55,encoding='unic')
+        size = font.getsize(char)
+        draw.text((startlen, 20),char,font=font,embedded_color=True)
+        startlen += size[0]
     with BytesIO() as image_binary:
             img.save(image_binary, 'PNG')
             image_binary.seek(0)
